baa05ff83de1940bb3297bbb1cffc7453d6c62f5
Job85 <julianjernigan@ymail.com>
2022-05-21 17:39:40 -0400

    updated route pathway names

--- server/routes/InventoryRouter.js
+++ server/routes/InventoryRouter.js
@@ -2,8 +2,8 @@ const Router = require('express').Router()
 const controller = require('../controllers/InventoryController')
 
 Router.get('/', controller.getInventory)
-Router.post(`/create_inventory`, controller.postInventory)
-Router.put(`/update_inventory`, controller.putInventory)
-Router.delete(`/delete_inventory`, controller.deleteInventory)
+Router.post(`/create`, controller.postInventory)
+Router.put(`/update`, controller.putInventory)
+Router.delete(`/delete`, controller.deleteInventory)
 
 module.exports = Router
