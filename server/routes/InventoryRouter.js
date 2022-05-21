1723091efb7022af82b3d9fd897aee9dafc92a04
Job85 <julianjernigan@ymail.com>
2022-05-21 17:19:17 -0400

    exported router

--- server/routes/InventoryRouter.js
+++ server/routes/InventoryRouter.js
@@ -5,3 +5,5 @@ Router.get('/', controller.getInventory)
 Router.post(`/create_inventory`, controller.postInventory)
 Router.put(`/update_inventory`, controller.putInventory)
 Router.delete(`/delete_inventory`, controller.deleteInventory)
+
+module.exports = Router
