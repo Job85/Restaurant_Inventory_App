e22825c34bcc658c2bccc5bacd97ca9913220e73
Job85 <julianjernigan@ymail.com>
2022-05-21 16:47:28 -0400

    ran seeder to seed demo item

--- server/seed/items.js
+++ server/seed/items.js
@@ -1,12 +1,23 @@
 const db = require('../db')
-const { Item } = require('../models')
+const { Item, Inventory } = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    const items = []
+    const demoInventory = await Inventory.find({ createdAt: "2022-05-21T20:14:18.795Z" })
+    const items = [
+        {
+            location: 'demo location',
+            category: 'demo category',
+            item: 'demo item',
+            size: 'demo size',
+            count: 12,
+            inventory_id: demoInventory[0]._id
+        }
+    ]
 
-    await Item.create(items)
+    await Item.insertMany(items)
+    console.log('Created items with inventories')
 }
 
 const run = async () => {
