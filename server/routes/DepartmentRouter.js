21a741f5661596f2020aeb26fe0114e77091e130
Job85 <julianjernigan@ymail.com>
2022-05-21 18:00:20 -0400

    added get department by id and updated delete route with id params

--- server/routes/DepartmentRouter.js
+++ server/routes/DepartmentRouter.js
@@ -2,8 +2,9 @@ const Router = require('express').Router()
 const controller = require('../controllers/DepartmentController')
 
 Router.get('/', controller.getDepartment)
+Router.get('/:id', controller.getDepartmentById)
 Router.post('/create', controller.createDepartment)
 Router.put('/update/:id', controller.updateDepartment)
-Router.delete('/delete', controller.deleteDepartment)
+Router.delete('/delete', controller.deleteDepartmentById)
 
 module.exports = Router
