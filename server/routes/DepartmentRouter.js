050ee7f6cdbce9f12a4fe0584849d1ffea5ebe4b
Job85 <julianjernigan@ymail.com>
2022-05-21 17:22:59 -0400

    updated route names

--- server/routes/DepartmentRouter.js
+++ server/routes/DepartmentRouter.js
@@ -1,9 +1,9 @@
 const Router = require('express').Router()
 const controller = require('../controllers/DepartmentController')
 
-Router.get('/', controller.GetDepartment)
-Router.post('/create_department', controller.CreateDepartment)
-Router.put('/update_department', controller.UpdateDepartment)
-Router.delete('/delete_department', controller.DeleteDepartment)
+Router.get('/', controller.getDepartment)
+Router.post('/create_department', controller.createDepartment)
+Router.put('/update_department', controller.updateDepartment)
+Router.delete('/delete_department', controller.deleteDepartment)
 
 module.exports = Router
