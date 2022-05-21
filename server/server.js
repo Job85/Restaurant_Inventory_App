f78fb1f4330d77397ff178d4dbb257d1981c46f5
Job85 <julianjernigan@ymail.com>
2022-05-21 17:32:57 -0400

    added AppRouter with api call

--- server/server.js
+++ server/server.js
@@ -8,18 +8,19 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(cors())
-app.use(express.static(`${__dirname}/client/build`))
+// app.use(express.static(`${__dirname}/client/build`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
+const AppRouter = require('./routes/AppRouter')
 
 app.get('/', (req, res) => {
     res.send({ msg: `Server Running on port ${PORT}` })
 })
 
 
-app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-})
-
+// app.get('/*', (req, res) => {
+//     res.sendFile(`${__dirname}/client/build/index.html`)
+// })
+app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
