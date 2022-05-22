9b844b5231e20c97e4bfb3b6194df9599688c7f1
Job85 <julianjernigan@ymail.com>
2022-05-21 21:29:31 -0400

    altered schema

--- server/models/inventory.js
+++ server/models/inventory.js
@@ -3,7 +3,9 @@ const { Schema } = require('mongoose')
 const Inventory = new Schema(
     {
         createdDate: { type: Date, require: true },
-        department_id: { type: Schema.Types.ObjectId, ref: 'department_id' }
+        department_id: { type: Schema.Types.ObjectId, ref: 'department_id' },
+        item_name: { type: String, require: true },
+        count: { type: Number, require: true }
     },
     { timestamps: true }
 )
