ed8335f39a22f976952a2cb24c8c8c6a3abaaf36
Job85 <julianjernigan@ymail.com>
2022-05-21 21:24:57 -0400

    altered schema

--- server/models/item.js
+++ server/models/item.js
@@ -5,10 +5,10 @@ const Item = new Schema(
         _id: { type: Schema.Types.ObjectId, default: null },
         location: { type: String, require: true },
         category: { type: String, require: true },
-        item: { type: String, require: true },
-        size: { type: String, require: true },
-        count: { type: Number, require: true },
-        inventory_id: { type: Schema.Types.ObjectId, ref: 'inventory_id' }
+        item_name: { type: String, require: true },
+        description: { type: String, require: true },
+        unit_measure: { type: String, require: true },
+        case_size: { type: String, require: true }
     },
     { timestamps: true }
 )
