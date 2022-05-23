8b989b583d638399a51abdf458eed9e88de5f12f
Job85 <julianjernigan@ymail.com>
2022-05-23 08:36:44 -0400

    reconfigured express.static & app.get /*

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const app = express()
 
 app.use(cors())
-app.use(express.static(path.resolve(`${__dirname}`, "./client/build")))
+app.use(express.static(`${__dirname}./client/build`))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}`, `/client/build`, `index.html`))
+    res.sendFile(`${__dirname}/client/buildindex.html`)
 })
 
 app.use('/api', AppRouter)
