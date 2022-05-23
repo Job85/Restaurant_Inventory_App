ad39aea2e7bba53c97a90683384aae70c5208787
Job85 <julianjernigan@ymail.com>
2022-05-22 22:24:40 -0400

    set up for deployment to heroku

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 
+const path = require('path')
+
 const app = express()
 
 app.use(cors())
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(path.resolve(`${__dirname}`, "./client/build")))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
@@ -20,7 +22,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
+    res.sendFile(path.resolve(`${__dirname}`, `/client/build`, `index.html`))
 })
 
 app.use('/api', AppRouter)
